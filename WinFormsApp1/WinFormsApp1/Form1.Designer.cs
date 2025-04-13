dc9d1b8476399ea84fa44f6d795ed27ad859a1ea
qwertyOko <Koval_wl91@student.itstep.org>
2025-04-14 02:42:05 +0300

    v0.0.2
    -add LinkLabel SignIn/SignUp
    -add LinkLabe FeedBack
    -fix button Upload Image
    -fix try-catch "Error"
    -update NameWindowApp
    -fix link for Api

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -1,71 +1,117 @@
-﻿namespace WinFormsApp1
+﻿using ReaLTaiizor.Controls;
+using System.Drawing;
+
+namespace WinFormsApp1
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private HopeButton selectButton;
+        private HopePictureBox imageBox;
+        private DungeonRichTextBox resultText;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            label1 = new Label();
+            selectButton = new HopeButton();
+            imageBox = new HopePictureBox();
+            resultText = new DungeonRichTextBox();
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)imageBox).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // selectButton
             // 
-            button1.Location = new Point(327, 334);
-            button1.Name = "button1";
-            button1.Size = new Size(172, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Выбрать изображение";
-            button1.UseVisualStyleBackColor = true;
+            selectButton.BorderColor = Color.FromArgb(220, 223, 230);
+            selectButton.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            selectButton.DangerColor = Color.FromArgb(245, 108, 108);
+            selectButton.DefaultColor = Color.FromArgb(255, 255, 255);
+            selectButton.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            selectButton.HoverTextColor = Color.FromArgb(48, 49, 51);
+            selectButton.InfoColor = Color.FromArgb(144, 147, 153);
+            selectButton.Location = new Point(330, 500);
+            selectButton.Name = "selectButton";
+            selectButton.PrimaryColor = Color.MediumSlateBlue;
+            selectButton.Size = new Size(240, 45);
+            selectButton.SuccessColor = Color.FromArgb(103, 194, 58);
+            selectButton.TabIndex = 2;
+            selectButton.Text = "Выбрать изображение";
+            selectButton.TextColor = Color.White;
+            selectButton.WarningColor = Color.FromArgb(230, 162, 60);
+            selectButton.Click += selectButton_Click;
             // 
-            // label1
+            // imageBox
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(311, 111);
-            label1.Name = "label1";
-            label1.Size = new Size(204, 15);
-            label1.TabIndex = 1;
-            label1.Text = "Распознание текста с изображения:";
+            imageBox.BackColor = Color.FromArgb(192, 196, 204);
+            imageBox.Location = new Point(50, 70);
+            imageBox.Name = "imageBox";
+            imageBox.PixelOffsetType = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            imageBox.Size = new Size(300, 300);
+            imageBox.SizeMode = PictureBoxSizeMode.Zoom;
+            imageBox.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            imageBox.TabIndex = 0;
+            imageBox.TabStop = false;
+            imageBox.TextRenderingType = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            // 
+            // resultText
+            // 
+            resultText.AutoWordSelection = false;
+            resultText.BackColor = Color.White;
+            resultText.BorderColor = Color.FromArgb(180, 180, 180);
+            resultText.EdgeColor = Color.White;
+            resultText.Font = new Font("Segoe UI", 10F);
+            resultText.ForeColor = Color.Black;
+            resultText.Location = new Point(380, 70);
+            resultText.Name = "resultText";
+            resultText.ReadOnly = true;
+            resultText.Size = new Size(470, 300);
+            resultText.TabIndex = 1;
+            resultText.Text = "Здесь будет результат...";
+            resultText.TextBackColor = Color.White;
+            resultText.WordWrap = true;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.BackColor = Color.Transparent;
+            linkLabel1.LinkColor = Color.White;
+            linkLabel1.Location = new Point(768, 27);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(107, 15);
+            linkLabel1.TabIndex = 3;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Вход/Регистрация";
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.BackColor = Color.Transparent;
+            linkLabel2.LinkColor = Color.White;
+            linkLabel2.Location = new Point(783, 576);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(92, 15);
+            linkLabel2.TabIndex = 4;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Оставить отзыв";
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(label1);
-            Controls.Add(button1);
+            BackColor = Color.FromArgb(32, 34, 37);
+            BackgroundImageLayout = ImageLayout.Stretch;
+            BackgroundImage = Image.FromFile("../../../bg1.jpg"); 
+            ClientSize = new Size(900, 600);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Controls.Add(imageBox);
+            Controls.Add(resultText);
+            Controls.Add(selectButton);
             Name = "Form1";
+            Text = "ScanText";
+            ((System.ComponentModel.ISupportInitialize)imageBox).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
-        #endregion
-
-
-        private Button button1;
-        private Label label1;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
     }
 }
